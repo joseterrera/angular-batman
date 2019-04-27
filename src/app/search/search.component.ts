b79fb3f7482a55e73f33f1018eaf2f218b474ff8
Jose Terrera <joseterrera@yahoo.com>
2019-04-27 15:57:29 -0700

    changed checkbox to radio buttons

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { StateService } from '../state.service';
 
+type Decade = '1990s' | '2000s';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  inputText = true;
-
+  decade: Decade = '1990s';
   constructor( private state: StateService ) {
     // console.log(this.inputText);
+    this.filterYears( this.decade );
   }
 
-  filterYears(value: boolean) {
+  filterYears(decade: Decade) {
     // We write a more elaborte mechanism that extracts all decades possible 
     // from the films, but for this example we'll restrict it to these decades
-    if ( value ) {
+    if ( decade === '2000s' ) {
       this.state.setState({ fromYear: 2000, toYear: 2999 });
     } else {
       this.state.setState({ fromYear: 1990, toYear: 1999 });
